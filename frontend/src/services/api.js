d948556d7412b5a3b97a5e6e4d8a99567b3922f6
Nitish Kumar <nitish.edu.in@gmail.com>
2026-07-15 01:59:51 +0530

    update API base URL to production server

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  // baseURL: "http://localhost:5000/api",
+  baseURL: "https://heart-disease-prediction-li1t.onrender.com/api",
+
   headers: {
     "Content-Type": "application/json",
   },
